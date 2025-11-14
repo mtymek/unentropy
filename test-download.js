ac0a7107a6573b02a5fc4d806683b6b60a4cef68
Mateusz Tymek <mtymek@gmail.com>
2025-11-14 20:12:23 +0100

    Test

--- test-download.js
+++ test-download.js
@@ -4,7 +4,7 @@ const client = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
-    bucket: process.env.AWS_BUCKET,
+    bucket: process.env.AWS_BUCKET_NAME,
     endpoint: process.env.AWS_ENDPOINT,
 });
 
