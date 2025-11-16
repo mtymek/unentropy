2766dd87ed98d774ec9470262805810664f3280f
Mateusz Tymek <mtymek@gmail.com>
2025-11-16 13:29:53 +0100

    debug messages

--- src/actions/track-metrics.ts
+++ src/actions/track-metrics.ts
@@ -34,7 +34,7 @@ interface ActionOutputs {
 
 function parseInputs(): ActionInputs {
   console.log('test');
-  console.log(core.getInput('s3-endpoint'));
+  console.log(" zz: " + core.getInput('s3-endpoint'));
   console.log(core.getInput('s3-access-key-id'));
   const storageType = core.getInput("storage-type") || "sqlite-local";
   const configFile = core.getInput("config-file") || "unentropy.json";
